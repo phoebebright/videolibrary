4e4f852453bacf6fa84167bb78d4b00fec0420c1
Phoebe B <phoebebright310@gmail.com>
2015-01-09 11:17:12 +0000

    automated duration and displayed filename

--- web/models.py
+++ web/models.py
@@ -25,7 +25,6 @@ class Library(models.Model):
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     added_on = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True, null=True)
-    duration = models.PositiveSmallIntegerField(max_length=3, default=1, help_text="Duration in Minutes")
     quality = models.CharField(max_length=10, blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     video = models.FileField(upload_to="uploads", help_text=".mp4 only")
